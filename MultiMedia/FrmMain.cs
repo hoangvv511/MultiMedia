6d8284fcca53e1480337ec90911686496527a2bc
BÌNH NGUYỄN VĂN <15520053@gm.uit.edu.vn>
2018-04-05 15:02:37 +0700

    dssadadsa41414

--- MultiMedia/FrmMain.cs
+++ MultiMedia/FrmMain.cs
@@ -62,6 +62,7 @@ namespace MultiMedia
                 btnMenu.Location = new Point(20, 58);
                 PanelAnimation2.ShowSync(Sidemenu);
             }
+            //qa
         }
     }
 }
